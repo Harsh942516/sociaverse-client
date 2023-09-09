4dcdf0ae3aa84698aacb0bab8305ad28f5317a5d
Harsh942516 <harshsingh9425168491@gmail.com>
2023-09-10 00:19:21 +0530

    adding frontend

---
+++ src/redux/slices/appConfigSlice.js
@@ -0,0 +1,107 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { axiosClient } from "../../utils/axiosClient";
+
+export const getMyInfo = createAsyncThunk(
+  "user/getMyInfo",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axiosClient.get("/user/getMyInfo");
+      return response.result;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+);
+
+export const updateMyProfile = createAsyncThunk(
+  "user/updateMyProfile",
+  async (body, thunkAPI) => {
+    try {
+      const response = await axiosClient.put("/user/", body);
+      return response.result;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+);
+
+const appConfigSlice = createSlice({
+  name: "appConfigSlice",
+  initialState: {
+    isLoading: false,
+    toastData: {},
+    myProfile: null,
+  },
+  reducers: {
+    setLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+    showToast: (state, action) => {
+      state.toastData = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getMyInfo.fulfilled, (state, action) => {
+        state.myProfile = action.payload.user;
+      })
+      .addCase(updateMyProfile.fulfilled, (state, action) => {
+        state.myProfile = action.payload.user;
+      });
+  },
+});
+
+export default appConfigSlice.reducer;
+
+export const { setLoading, showToast } = appConfigSlice.actions;
+
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import { axiosClient } from "../../utils/axiosClient";
+
+// export const getMyInfo = createAsyncThunk("user/getMyInfo", async () => {
+//   try {
+//     const response = await axiosClient.get("/user/getMyInfo");
+//     return response.result;
+//   } catch (error) {
+//     return Promise.reject(error);
+//   }
+// });
+
+// export const updateMyProfile = createAsyncThunk(
+//   "user/updateMyProfile",
+//   async (body) => {
+//     try {
+//       const response = await axiosClient.put("/user/", body);
+//       return response.result;
+//     } catch (error) {
+//       return Promise.reject(error);
+//     }
+//   }
+// );
+
+// const appConfigSlice = createSlice({
+//   name: "appConfigSlice",
+//   initialState: {
+//     isLoading: false,
+//     toastData: {},
+//     myProfile: null,
+//   },
+//   reducers: {
+//     setLoading: (state, action) => {
+//       state.isLoading = action.payload;
+//     },
+//   },
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(getMyInfo.fulfilled, (state, action) => {
+//         state.myProfile = action.payload.user;
+//       })
+//       .addCase(updateMyProfile.fulfilled, (state, action) => {
+//         state.myProfile = action.payload.user;
+//       });
+//   },
+// });
+
+// export default appConfigSlice.reducer;
+
+// export const { setLoading } = appConfigSlice.actions;
